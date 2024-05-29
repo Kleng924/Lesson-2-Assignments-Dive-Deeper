dd799aeb4bfdb247599f7a813c559e5725fcfe54
Kleng924 <mlavilla924@gmail.com>
2024-05-28 19:28:13 -0700

    First commit

---
+++ The Greatest Showdown.py
@@ -0,0 +1,34 @@
+# Question 2 Task 1
+
+number = int(input("Enter a number(5-7):"))
+
+if number == 5:
+    print("The number is the smallest.")
+elif number == 7:
+    print("The number is in the greatest.")
+else:
+    print("The number is in between.")
+
+# Question 2 Task 2
+
+number = int(input("Enter a number(9-11):"))
+
+if number == 9:
+    print("The number is the smallest.")
+elif number == 11:
+    print("The number is in the greatest.")
+else:
+    print("The number is in between.")
+
+#Question 2 Task 3
+
+numbers = int(input("3, 3, 4:"))
+
+if number == 3:
+    print("The number is eqaul to 3.")
+
+elif number <= 4:
+    print("The number is the largest.")
+
+else:
+    print("All numbers are equal.")
